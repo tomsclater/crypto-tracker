1e35deb392d87bb5ba9c879a5b3169eeb9992752
Thomas <tomsclater@live.com>
2022-06-28 20:49:25 -0700

    Added image and text

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ color: "white" }}>Hello World!</Text>
+      <Image
+        source={{
+          uri: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
+        }}
+        style={{ height: 30, width: 30 }}
+      />
+      <Text style={styles.title}>Bitcoin</Text>
       <StatusBar style="light" />
     </View>
   );
@@ -14,7 +20,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#121212",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 50,
+  },
+  title: {
+    color: "white",
+    fontSize: 16,
   },
 });
