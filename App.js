a80e59f1f001403116eef547efb107a30cc6c0db
Thomas <tomsclater@live.com>
2022-06-28 21:10:57 -0700

    Added styling

--- App.js
+++ App.js
@@ -4,18 +4,25 @@ import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image
-        source={{
-          uri: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
-        }}
-        style={{ height: 30, width: 30 }}
-      />
-      <View>
-        <Text style={styles.title}>Bitcoin</Text>
-        <View style={{ flexDirection: "row" }}>
-          <Text style={styles.title}>1</Text>
-          <Text style={styles.title}>BTC</Text>
-          <Text style={styles.title}>0.63%</Text>
+      <View style={styles.coinContainer}>
+        <Image
+          source={{
+            uri: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
+          }}
+          style={{
+            height: 30,
+            width: 30,
+            marginRight: 10,
+            alignSelf: "center",
+          }}
+        />
+        <View>
+          <Text style={styles.title}>Bitcoin</Text>
+          <View style={{ flexDirection: "row" }}>
+            <Text style={styles.text}>1</Text>
+            <Text style={styles.text}>BTC</Text>
+            <Text style={styles.text}>0.63%</Text>
+          </View>
         </View>
         <StatusBar style="light" />
       </View>
@@ -35,6 +42,13 @@ const styles = StyleSheet.create({
     fontWeight: "800",
   },
   text: {
-    color: "white",
+    color: "#FFF0F5",
+    marginRight: 5,
+  },
+  coinContainer: {
+    flexDirection: "row",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: "#282828",
+    padding: 15,
   },
 });
