4462d67deba4a9528c2171de42e82dd1502a69fd
Thomas <tomsclater@live.com>
2022-06-29 15:16:20 -0700

    Added dynamic data for detailed screen

--- src/screens/CoinDetailedScreen/index.jsx
+++ src/screens/CoinDetailedScreen/index.jsx
@@ -1,12 +1,21 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import Coin from "../../../assets/data/crypto.json";
 
 const CoinDetailedScreen = () => {
+  const {
+    image: { small },
+    name,
+    market_data: { market_cap_rank },
+  } = Coin;
   return (
     <View>
       <Text style={{ color: "white" }}>Detailed Screen</Text>
       <Ionicons name="chevron-back-sharp" size={24} color="white" />
+      <Image source={{ uri: small }} style={{ width: 25, height: 25 }} />
+      <Text style={{ color: "white" }}>{name}</Text>
+      <Text style={{ color: "white" }}>#{market_cap_rank}</Text>
     </View>
   );
 };
