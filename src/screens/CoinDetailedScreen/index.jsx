536caa7049de18c9caf2ac3bae53a034e11996d7
Thomas <tomsclater@live.com>
2022-07-01 10:28:28 -0700

    Styling detailed screen header

--- src/screens/CoinDetailedScreen/index.jsx
+++ src/screens/CoinDetailedScreen/index.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import { View, Text, Image } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import { Ionicons, EvilIcons } from "@expo/vector-icons";
 import Coin from "../../../assets/data/crypto.json";
+import styles from "./styles";
 
 const CoinDetailedScreen = () => {
   const {
     image: { small },
     name,
+    symbol,
     market_data: { market_cap_rank },
   } = Coin;
+
   return (
-    <View>
-      <Text style={{ color: "white" }}>Detailed Screen</Text>
+    <View style={styles.headerContainer}>
+      {/* <Text style={{ color: "white" }}>Detailed Screen</Text> */}
       <Ionicons name="chevron-back-sharp" size={24} color="white" />
-      <Image source={{ uri: small }} style={{ width: 25, height: 25 }} />
-      <Text style={{ color: "white" }}>{name}</Text>
-      <Text style={{ color: "white" }}>#{market_cap_rank}</Text>
+      <View style={styles.tickerContainer}>
+        <Image source={{ uri: small }} style={{ width: 25, height: 25 }} />
+        <Text style={{ color: "white" }}>{symbol.toUpperCase()}</Text>
+        <Text style={{ color: "white" }}>#{market_cap_rank}</Text>
+      </View>
+      <EvilIcons name="user" size={30} color="white" />
     </View>
   );
 };
---
+++ src/screens/CoinDetailedScreen/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: "row",
+    paddingHorizontal: 10,
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  tickerContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+});
+
+export default styles;
