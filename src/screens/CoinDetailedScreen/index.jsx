cfb555db69a3fe3abbcfe21d629be27341e0f464
Thomas <tomsclater@live.com>
2022-07-03 23:15:02 -0700

    Implementing axios

--- src/screens/CoinDetailedScreen/index.jsx
+++ src/screens/CoinDetailedScreen/index.jsx
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, Dimensions, TextInput } from "react-native";
-import Coin from "../../../assets/data/crypto.json";
+import {
+  View,
+  Text,
+  Dimensions,
+  TextInput,
+  ActivityIndicator,
+} from "react-native";
 import CoinDetailedHeader from "./components/CoinDetailedHeader";
 import styles from "./styles";
 import { AntDesign } from "@expo/vector-icons";
@@ -11,36 +16,57 @@ import {
   ChartYLabel,
 } from "@rainbow-me/animated-charts";
 import { useRoute } from "@react-navigation/native";
+import {
+  getDetailedCoinData,
+  getCoinMarketChart,
+} from "../../services/requests";
 
 const CoinDetailedScreen = () => {
+  const [coin, setCoin] = useState(null);
+  const [coinMarketData, setCoinMarketData] = useState(null);
+
+  const route = useRoute();
+  const {
+    params: { coinId },
+  } = route;
+  const [loading, setLoading] = useState(false);
+
+  const fetchCoinData = async () => {
+    setLoading(true);
+    const fetchedCoinData = await getDetailedCoinData(coinId);
+    const fetchedCoinMarketData = await getCoinMarketChart(coinId);
+    setCoin(fetchedCoinData);
+    setCoinMarketData(fetchedCoinMarketData);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchCoinData();
+  }, []);
+
+  if (loading || !coin || !coinMarketData) {
+    return <ActivityIndicator size="large" />;
+  }
+
   const {
     image: { small },
     name,
     symbol,
-    prices,
     market_data: {
       market_cap_rank,
       current_price,
       price_change_percentage_24h,
     },
-  } = Coin;
+  } = coin;
+
+  const { prices } = coinMarketData;
 
   const [coinValue, setCoinValue] = useState("1");
   const [usdValue, setUsdValue] = useState(current_price.usd.toString());
 
-  const route = useRoute();
-
-  const {
-    params: { coinId },
-  } = route;
-
-  console.log(coinId);
-
   const percentageColor =
     price_change_percentage_24h < 0 ? "#ea3943" : "#16c784";
-
   const chartColor = current_price.usd > prices[0][1] ? "#16c784" : "#ea3943";
-
   const screenWidth = Dimensions.get("window").width;
 
   const formatCurrency = (value) => {
