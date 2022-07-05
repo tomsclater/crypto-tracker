a822a5d956333cfa47859656d123d0178d9f80d5
Thomas <tomsclater@live.com>
2022-07-04 22:43:11 -0700

    Styling bottom bar

--- src/navigation/BottomTabNavigator.jsx
+++ src/navigation/BottomTabNavigator.jsx
@@ -1,13 +1,35 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/HomeScreen";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name={"Home"} component={HomeScreen} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "white",
+        tabBarInactiveTintColor: "grey",
+        tabBarStyle: {
+          backgroundColor: "#303030",
+        },
+      }}
+    >
+      <Tab.Screen
+        name={"Home"}
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ focused, color }) => (
+            <MaterialCommunityIcons
+              name="home-circle-outline"
+              size={focused ? 40 : 25}
+              color={color}
+            />
+          ),
+        }}
+      />
       {/* <Tab.Screen /> */}
     </Tab.Navigator>
   );
