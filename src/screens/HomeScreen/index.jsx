83f36fc83b9acfb6c16fd302d69f8a1e1596f19c
Thomas <tomsclater@live.com>
2022-07-05 23:58:59 -0700

    Refresh list when scrolling down from top

--- src/screens/HomeScreen/index.jsx
+++ src/screens/HomeScreen/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { FlatList } from "react-native";
+import { FlatList, RefreshControl } from "react-native";
 import CoinItem from "../../components/CoinItem";
 import { getMarketData } from "../../services/requests";
 
@@ -14,13 +14,28 @@ const HomeScreen = () => {
     setLoading(false);
   };
 
+  const refetchCoins = async () => {
+    setLoading(true);
+    const coinsData = await getMarketData();
+    setCoins(coinsData);
+    setLoading(false);
+  };
+
   useEffect(() => {
     fetchCoins();
   }, []);
+
   return (
     <FlatList
       data={coins}
       renderItem={({ item }) => <CoinItem marketCoin={item} />}
+      refreshControl={
+        <RefreshControl
+          refreshing={loading}
+          tintColor="white"
+          onRefresh={refetchCoins}
+        />
+      }
     />
   );
 };
