0f2e878179cc23eef472c8d111eb16b888017b01
Thomas <tomsclater@live.com>
2022-07-11 21:52:10 -0700

    Styling portfolio

--- src/screens/PortfolioScreen/components/PortfolioAssetsList/index.jsx
+++ src/screens/PortfolioScreen/components/PortfolioAssetsList/index.jsx
@@ -1,10 +1,31 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import styles from "./styles";
 
 const PortfolioAssetsList = () => {
   return (
     <View>
-      <Text style={{ color: "white" }}>Portfolio Assets List</Text>
+      <FlatList
+        data={[]}
+        renderItem={() => <Text>Item</Text>}
+        ListHeaderComponent={
+          <>
+            <Text style={styles.currentBalance}>Current Balance</Text>
+            <Text style={styles.currentBalanceValue}>$20000</Text>
+            <Text style={styles.valueChange}>1000 (All Time)</Text>
+            <View>
+              <AntDesign
+                style={{ alignSelf: "center", marginRight: 5 }}
+                name={"caretup"}
+                size={12}
+                color={"white"}
+              />
+              <Text style={styles.percentageChange}>1.2%</Text>
+            </View>
+          </>
+        }
+      />
     </View>
   );
 };
---
+++ src/screens/PortfolioScreen/components/PortfolioAssetsList/styles.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  currentBalance: {
+    color: "white",
+    fontWeight: "600",
+    fontSize: 15,
+  },
+  currentBalanceValue: {
+    color: "white",
+    fontSize: 40,
+    fontWeight: "700",
+    letterSpacing: 1,
+  },
+  valueChange: {
+    fontWeight: "600",
+    fontSize: 16,
+    color: "white",
+  },
+  percentageChange: {
+    color: "white",
+    fontWeight: "500",
+    fontSize: 17,
+  },
+});
+
+export default styles;
