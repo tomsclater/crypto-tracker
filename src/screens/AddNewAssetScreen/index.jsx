e37e762f871d14da508ecf3cfc9087f021b5d9be
Thomas <tomsclater@live.com>
2022-07-12 23:47:49 -0700

    Added searchable drop-down

--- src/screens/AddNewAssetScreen/index.jsx
+++ src/screens/AddNewAssetScreen/index.jsx
@@ -1,10 +1,41 @@
 import React from "react";
 import { View, Text } from "react-native";
+import SearchableDropDown from "react-native-searchable-dropdown";
 
 const AddNewAssetScreen = () => {
   return (
     <View>
-      <Text style={{ color: "white" }}>AddNewAssetScreen</Text>
+      <SearchableDropDown
+        containerStyle={{
+          width: "100%",
+          paddingHorizontal: 10,
+          paddingVertical: 20,
+        }}
+        itemStyle={{
+          padding: 10,
+          marginTop: 2,
+          backgroundColor: "#1e1e1e",
+          borderWidth: 1,
+          borderColor: "#444444",
+          borderRadius: 5,
+        }}
+        itemTextStyle={{ color: "white" }}
+        items={[]}
+        resetValue={false}
+        placeholder={"Select a coin..."}
+        placeholderTextColor="white"
+        textInputProps={{
+          underlineColorAndroid: "transparent",
+          style: {
+            padding: 12,
+            borderWidth: 1.5,
+            borderColor: "#444444",
+            borderRadius: 5,
+            backgroundColor: "#1e1e1e",
+            color: "white",
+          },
+        }}
+      />
     </View>
   );
 };
