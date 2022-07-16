b1d50622e37991f4373e82ff9aef884ee7314ab4
Thomas <tomsclater@live.com>
2022-07-16 02:51:46 -0700

    Creating list of assets

--- src/screens/AddNewAssetScreen/index.jsx
+++ src/screens/AddNewAssetScreen/index.jsx
@@ -1,31 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, TextInput, Pressable } from "react-native";
 import SearchableDropDown from "react-native-searchable-dropdown";
 import styles from "./styles";
-import { useRecoilState } from 'recoil';
-import { allPortfolioBoughtAssetsInStorage } from '../../atoms/PortfolioAssets';
+import { useRecoilState } from "recoil";
+import { allPortfolioBoughtAssetsInStorage } from "../../atoms/PortfolioAssets";
+import { getAllCoins } from "../../services/requests";
 
 const AddNewAssetScreen = () => {
-  const [allCoins, setAllCoins] = useState([])
+  const [allCoins, setAllCoins] = useState([]);
   const [boughtAssetQuantity, setBoughtAssetQuantity] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [selectedCoinId, setSelectedCoinId] = useState(false);
+
   const [assetsInStorage, setAssetsInStorage] = useRecoilState(
     allPortfolioBoughtAssetsInStorage
   );
 
-  const onAddNewAsset = () => {
+  const onAddNewAsset = () => {};
 
-  }
+  const fetchAllCoins = async () => {
+    if (loading) {
+      return;
+    }
+    setLoading(true);
+    const allCoins = await getAllCoins();
+    setAllCoins(allCoins);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchAllCoins();
+  }, []);
 
   return (
     <View style={{ flex: 1 }}>
       <SearchableDropDown
         items={allCoins}
-        onItemSelect={(item) => console.log(item)}
+        onItemSelect={(item) => setSelectedCoinId(item.id)}
         containerStyle={styles.dropdownContainer}
         itemStyle={styles.item}
         itemTextStyle={{ color: "white" }}
         resetValue={false}
-        placeholder={"Select a coin..."}
+        placeholder={selectedCoinId || "Select a coin..."}
         placeholderTextColor="white"
         textInputProps={{
           underlineColorAndroid: "transparent",
@@ -39,25 +55,26 @@ const AddNewAssetScreen = () => {
           },
         }}
       />
-      <View style={styles.boughtQuantityContainer}>
-        <View style={{ flexDirection: "row" }}>
-          <TextInput
-            style={{ color: "white", fontSize: 90 }}
-            value={boughtAssetQuantity}
-            placeholder="0"
-            keyboardType="numeric"
-            onChangeText={setBoughtAssetQuantity}
-          />
-          <Text style={styles.ticker}>BTC</Text>
-        </View>
-        <Text style={styles.pricePerCoin}>$20000 per coin</Text>
-      </View>
-      <Pressable
-        style={styles.buttonContainer}
-        onPress={onAddNewAsset}
-      >
-        <Text style={styles.buttonText}>Add New Asset</Text>
-      </Pressable>
+      {selectedCoinId && (
+        <>
+          <View style={styles.boughtQuantityContainer}>
+            <View style={{ flexDirection: "row" }}>
+              <TextInput
+                style={{ color: "white", fontSize: 90 }}
+                value={boughtAssetQuantity}
+                placeholder="0"
+                keyboardType="numeric"
+                onChangeText={setBoughtAssetQuantity}
+              />
+              <Text style={styles.ticker}>BTC</Text>
+            </View>
+            <Text style={styles.pricePerCoin}>$20000 per coin</Text>
+          </View>
+          <Pressable style={styles.buttonContainer} onPress={onAddNewAsset}>
+            <Text style={styles.buttonText}>Add New Asset</Text>
+          </Pressable>
+        </>
+      )}
     </View>
   );
 };
